98755155ca21d943925756e1603731244e03e4cb
Ngoc Tran <ngoctran180199@gmail.com>
2022-08-18 22:24:27 +0700

    debug code

--- app.js
+++ app.js
@@ -130,7 +130,11 @@ app.get("/work", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+const port = process.env.PORT;
+if(port === null || port === ""){
+  port = 3000;
+}
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
